7e9921e9f18f3e298554c0cf3d1994ac49401fed
Mathieu Joly <mathieujoly@MacBook-Pro-de-Mathieu.local>
2024-12-16 23:48:15 +0100

    add fully working solution for day03 part a

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,10 +1,27 @@
 advent_of_code::solution!(3);
 
-pub fn part_one(input: &str) -> Option<u64> {
-    None
+use regex::Regex;
+
+fn parse_input(input: &str) -> String {
+    input.replace("\n", "")
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+pub fn part_one(input: &str) -> Option<i32> {
+    let one_line = parse_input(input);
+
+    let mut total: i32 = 0;
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    for cap in re.captures_iter(&one_line) {
+        let a: i32 = cap[1].parse().unwrap();
+        let b: i32 = cap[2].parse().unwrap();
+        total += a * b;
+    }
+
+    Some(total)
+}
+
+pub fn part_two(_input: &str) -> Option<u64> {
     None
 }
 
@@ -15,7 +32,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(161));
     }
 
     #[test]
