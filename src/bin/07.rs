5f768fcc66d75604a5eccb24a0affc93e69d2c75
Mathieu Joly <mathieujoly@MacBook-Pro-de-Mathieu.local>
2024-12-17 21:51:07 +0100

    add fully working solution for day 07 part a

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,10 +1,48 @@
 advent_of_code::solution!(7);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let data = line.split(": ").collect::<Vec<&str>>();
+        let target = data[0].parse().unwrap();
+        let values: Vec<u64> = data[1]
+            .trim()
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+
+        let mut results = Vec::new();
+        results.push(values[0]);
+
+        for i in 1..values.len() {
+            let mut new_results = Vec::new();
+            let curr = values[i];
+
+            for result in &results {
+                let sum = result + curr;
+                if sum <= target {
+                    new_results.push(sum);
+                }
+
+                let product = result * curr;
+                if product <= target {
+                    new_results.push(product);
+                }
+            }
+
+            results = new_results;
+        }
+
+        if results.contains(&target) {
+            sum += target;
+        }
+    }
+
+    Some(sum)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+pub fn part_two(_input: &str) -> Option<u64> {
     None
 }
 
@@ -15,7 +53,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY, None));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3749));
     }
 
     #[test]
