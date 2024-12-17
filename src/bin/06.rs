307e50eb40f5cd94eae45d5ac617463faa1e22f4
Mathieu Joly <mathieujoly@MacBook-Pro-de-Mathieu.local>
2024-12-17 18:13:52 +0100

    add fully working non efficient solution for day 06 part b

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,131 +1,140 @@
-use std::collections::HashMap;
-
 advent_of_code::solution!(6);
 
-fn count_visited_cells(
-    grid: &Vec<Vec<char>>,
-    height: usize,
-    width: usize,
-    mut pos: (usize, usize),
-    mut dir: (i32, i32),
-    new_wall: Option<(usize, usize)>,
-) -> i32 {
-    let mut visited: HashMap<(usize, usize), String> = HashMap::new();
+fn sum_visited(visited: Vec<Vec<Vec<char>>>) -> i32 {
+    let mut count = 0;
 
-    loop {
-        let next_x = pos.1 as i32 + dir.1;
-        let next_y = pos.0 as i32 + dir.0;
-
-        // check if the robot is still on the grid
-        if next_y < 0 || next_y >= height as i32 || next_x < 0 || next_x >= width as i32 {
-            break;
-        }
-
-        let next = match new_wall {
-            Some((x, y)) => {
-                if x == next_x as usize && y == next_y as usize {
-                    '#'
-                } else {
-                    grid[next_y as usize][next_x as usize]
-                }
+    for y in 0..visited.len() {
+        for x in 0..visited[y].len() {
+            if visited[y][x].len() > 0 {
+                count += 1;
             }
-            None => grid[next_y as usize][next_x as usize],
-        };
-
-        // turn clockwise if next is a wall (#)
-        if next == '#' {
-            if dir.1 == 0 {
-                dir.1 = -dir.0;
-                dir.0 = 0;
-            } else {
-                dir.0 = dir.1;
-                dir.1 = 0;
-            }
-        } else {
-            // move the robot
-            pos.1 = next_x as usize;
-            pos.0 = next_y as usize;
-        }
-
-        // mark the cell as visited
-        let sign = if dir.1 == 0 {
-            if dir.0 == -1 {
-                '^'
-            } else {
-                'v'
-            }
-        } else {
-            if dir.1 == -1 {
-                '<'
-            } else {
-                '>'
-            }
-        };
-
-        if visited.contains_key(&(pos.1, pos.0)) {
-            let visits = visited.get_mut(&(pos.1, pos.0)).unwrap();
-            if visits.contains(sign) {
-                return -1;
-            } else {
-                visits.push(sign);
-            }
-        } else {
-            visited.insert((pos.1, pos.0), sign.to_string());
         }
     }
 
-    // return the number of visited cells
-    visited.len() as i32
+    count
+}
+
+fn count_visited_cells(
+    grid: Vec<Vec<char>>,
+    mut x: usize,
+    mut y: usize,
+    mut direction: char,
+) -> i32 {
+    let mut visited: Vec<Vec<Vec<char>>> = vec![vec![vec![]; grid[0].len()]; grid.len()];
+
+    loop {
+        if visited[y][x].contains(&direction) {
+            return -1;
+        } else {
+            visited[y][x].push(direction);
+        }
+
+        match direction {
+            '>' => {
+                if x + 1 == grid[y].len() {
+                    return sum_visited(visited);
+                } else if grid[y][x + 1] == '#' {
+                    direction = 'v';
+                } else {
+                    x += 1;
+                }
+            }
+            '<' => {
+                if x == 0 {
+                    return sum_visited(visited);
+                } else if grid[y][x - 1] == '#' {
+                    direction = '^';
+                } else {
+                    x -= 1;
+                }
+            }
+            '^' => {
+                if y == 0 {
+                    return sum_visited(visited);
+                } else if grid[y - 1][x] == '#' {
+                    direction = '>';
+                } else {
+                    y -= 1;
+                }
+            }
+            _ => {
+                if y + 1 == grid.len() {
+                    return sum_visited(visited);
+                } else if grid[y + 1][x] == '#' {
+                    direction = '<';
+                } else {
+                    y += 1;
+                }
+            }
+        }
+    }
+}
+
+fn get_robot_data(grid: &Vec<Vec<char>>, height: usize, width: usize) -> (usize, usize, char) {
+    let mut pos_x = 0;
+    let mut pos_y = 0;
+    let mut dir = '>';
+
+    for y in 0..height {
+        for x in 0..width {
+            let curr = grid[y][x];
+
+            if curr != '^' && curr != '>' && curr != 'v' && curr != '<' {
+                continue;
+            }
+
+            dir = curr;
+            pos_x = x;
+            pos_y = y;
+
+            break;
+        }
+    }
+
+    (pos_x, pos_y, dir)
 }
 
 pub fn part_one(input: &str) -> Option<i32> {
-    let mut grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-
+    // grid data
+    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
     let height = grid.len();
     let width = grid[0].len();
 
     // robot data
-    let mut pos_x = 0;
-    let mut pos_y = 0;
-    let mut dir_x = 0;
-    let mut dir_y = 0;
+    let (pos_x, pos_y, dir) = get_robot_data(&grid, height, width);
 
-    // init robot
-    for y in 0..height {
-        for x in 0..width {
-            match grid[y][x] {
-                '^' => {
-                    dir_x = 0;
-                    dir_y = -1;
-                }
-                '>' => {
-                    dir_x = 1;
-                    dir_y = 0;
-                }
-                'v' => {
-                    dir_x = 0;
-                    dir_y = 1;
-                }
-                '<' => {
-                    dir_x = -1;
-                    dir_y = 0;
-                }
-                _ => continue,
-            }
-
-            pos_x = x;
-            pos_y = y;
-            grid[y][x] = 'X';
-        }
-    }
-
-    let count = count_visited_cells(&grid, height, width, (pos_y, pos_x), (dir_y, dir_x), None);
+    let count = count_visited_cells(grid, pos_x, pos_y, dir);
 
     Some(count)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<i32> {
+    // grid data
+    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let height = grid.len();
+    let width = grid[0].len();
+
+    // robot data
+    let (pos_x, pos_y, dir) = get_robot_data(&grid, height, width);
+
+    let mut count = 0;
+
+    for y in 0..height {
+        for x in 0..width {
+            if grid[y][x] != '.' {
+                continue;
+            }
+
+            let mut new_grid = grid.clone();
+            new_grid[y][x] = '#';
+
+            if count_visited_cells(new_grid, pos_x, pos_y, dir) == -1 {
+                count += 1;
+            }
+        }
+    }
+
+    Some(count)
 }
 
 #[cfg(test)]
@@ -141,6 +150,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY, None));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
