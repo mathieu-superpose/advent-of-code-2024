12349150be53c720cc61c5f2860673a58c0f1527
Mathieu Joly <mathieujoly@MacBook-Pro-de-Mathieu.local>
2024-12-17 01:41:55 +0100

    add fully working solution for day 04 part a

---
+++ src/bin/04.rs
@@ -0,0 +1,69 @@
+advent_of_code::solution!(4);
+
+pub fn part_one(input: &str) -> Option<i32> {
+    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+
+    // count the number of occurence of the word XMAS in the grid in all directions
+    let mut count = 0;
+
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            let curr = grid[i][j];
+
+            if curr != 'X' {
+                continue;
+            }
+
+            for (dir_y, dir_x) in vec![
+                (-1, -1), // top left
+                (-1, 0),  // top
+                (-1, 1),  // top right
+                (0, -1),  // left
+                (0, 1),   // right
+                (1, -1),  // bottom right
+                (1, 0),   // bottom
+                (1, 1),   // bottom left
+            ] {
+                let mut xmas: String = String::new();
+
+                for index in 0..=3 {
+                    let y = i as i32 + dir_y * index;
+                    let x = j as i32 + dir_x * index;
+
+                    if y < 0 || y >= grid.len() as i32 || x < 0 || x >= grid[i].len() as i32 {
+                        break;
+                    }
+
+                    xmas.push(grid[y as usize][x as usize]);
+                }
+
+                if xmas == "XMAS" {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    Some(count)
+}
+
+pub fn part_two(_input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY, None));
+        assert_eq!(result, Some(18));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY, None));
+        assert_eq!(result, None);
+    }
+}
