ae801945c240f6882a772afdcc02bd656cdff9d3
Mathieu Joly <mathieujoly@MacBook-Pro-de-Mathieu.local>
2024-12-17 18:27:02 +0100

    update day 06 part b with a most efficient solution

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 advent_of_code::solution!(6);
 
 fn sum_visited(visited: Vec<Vec<Vec<char>>>) -> i32 {
@@ -14,17 +16,17 @@ fn sum_visited(visited: Vec<Vec<Vec<char>>>) -> i32 {
     count
 }
 
-fn count_visited_cells(
-    grid: Vec<Vec<char>>,
+fn get_visited_cells(
+    grid: &Vec<Vec<char>>,
     mut x: usize,
     mut y: usize,
     mut direction: char,
-) -> i32 {
+) -> Vec<Vec<Vec<char>>> {
     let mut visited: Vec<Vec<Vec<char>>> = vec![vec![vec![]; grid[0].len()]; grid.len()];
 
     loop {
         if visited[y][x].contains(&direction) {
-            return -1;
+            return vec![vec![]];
         } else {
             visited[y][x].push(direction);
         }
@@ -32,7 +34,7 @@ fn count_visited_cells(
         match direction {
             '>' => {
                 if x + 1 == grid[y].len() {
-                    return sum_visited(visited);
+                    return visited;
                 } else if grid[y][x + 1] == '#' {
                     direction = 'v';
                 } else {
@@ -41,7 +43,7 @@ fn count_visited_cells(
             }
             '<' => {
                 if x == 0 {
-                    return sum_visited(visited);
+                    return visited;
                 } else if grid[y][x - 1] == '#' {
                     direction = '^';
                 } else {
@@ -50,7 +52,7 @@ fn count_visited_cells(
             }
             '^' => {
                 if y == 0 {
-                    return sum_visited(visited);
+                    return visited;
                 } else if grid[y - 1][x] == '#' {
                     direction = '>';
                 } else {
@@ -59,7 +61,7 @@ fn count_visited_cells(
             }
             _ => {
                 if y + 1 == grid.len() {
-                    return sum_visited(visited);
+                    return visited;
                 } else if grid[y + 1][x] == '#' {
                     direction = '<';
                 } else {
@@ -103,33 +105,37 @@ pub fn part_one(input: &str) -> Option<i32> {
     // robot data
     let (pos_x, pos_y, dir) = get_robot_data(&grid, height, width);
 
-    let count = count_visited_cells(grid, pos_x, pos_y, dir);
+    let visited = get_visited_cells(&grid, pos_x, pos_y, dir);
+    let count = sum_visited(visited);
 
     Some(count)
 }
 
 pub fn part_two(input: &str) -> Option<i32> {
     // grid data
-    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let mut grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
     let height = grid.len();
     let width = grid[0].len();
 
     // robot data
     let (pos_x, pos_y, dir) = get_robot_data(&grid, height, width);
 
+    let visited = get_visited_cells(&grid, pos_x, pos_y, dir);
     let mut count = 0;
 
     for y in 0..height {
         for x in 0..width {
-            if grid[y][x] != '.' {
+            if y == pos_y && x == pos_x {
                 continue;
             }
 
-            let mut new_grid = grid.clone();
-            new_grid[y][x] = '#';
-
-            if count_visited_cells(new_grid, pos_x, pos_y, dir) == -1 {
-                count += 1;
+            if visited[y][x].len() > 0 {
+                grid[y][x] = '#';
+                let v = get_visited_cells(&grid, pos_x, pos_y, dir);
+                if v.len() == 1 {
+                    count += 1;
+                }
+                grid[y][x] = '.';
             }
         }
     }
