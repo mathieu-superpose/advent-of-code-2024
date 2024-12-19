5f34404fc6bbbd5a511d870d346c8c13394659b8
Mathieu Joly <mathieujoly@MacBook-Pro-de-Mathieu.local>
2024-12-19 10:10:54 +0100

    add example working solution for day 20 part b

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -58,15 +58,52 @@ pub fn part_one(input: &str) -> Option<i32> {
 
         if possible {
             count += 1;
-        } else {
         }
     }
 
     Some(count)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let data: Vec<&str> = input.split("\n\n").collect();
+    let data_towels = data[0].trim().split(", ").collect::<Vec<&str>>();
+    let data_design = data[1];
+
+    let mut possible_designs: HashSet<String> = HashSet::new();
+
+    for towel in data_towels {
+        possible_designs.insert(towel.to_string());
+    }
+
+    let check_possible = |design: &str, start: usize, end: usize| -> bool {
+        let d = (&design[start..end]).to_string();
+        possible_designs.contains(&d)
+    };
+
+    let mut count = 0;
+
+    for d in data_design.lines() {
+        let design: &str = d.trim();
+
+        let mut stack: Vec<(usize, usize)> = Vec::new();
+        stack.push((0, design.len()));
+
+        while stack.len() > 0 {
+            let (start, end) = stack.pop().unwrap();
+
+            for i in start + 1..=end {
+                if check_possible(design, start, i) {
+                    if i == design.len() {
+                        count += 1;
+                    } else {
+                        stack.push((i, end));
+                    }
+                }
+            }
+        }
+    }
+
+    Some(count)
 }
 
 #[cfg(test)]
@@ -82,6 +119,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY, None));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(16));
     }
 }
